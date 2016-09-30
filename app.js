98a2ea15bfcc842ca4d2719c6d84fd8e8869d086
Nicko <nicolasmathieu31@hotmail.com>
2016-09-30 12:33:29 +0200

    jq-greetings fini

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('have fun !');
+$("button").click(function(){
+
+	var obj = {
+
+		prenom : $("#first_name").val(),
+		nom : $("#last_name").val(),
+
+
+	};
+
+	console.log(obj);	
+
+	$("#username").text(obj.prenom + " " + obj.nom);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
